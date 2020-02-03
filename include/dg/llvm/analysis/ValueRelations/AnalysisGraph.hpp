c13ae58e6f42de315915e691677e550cdaec75fd
Anna Rechtackova <xrechtac@fi.muni.cz>
2020-02-03 14:33:29 +0100

    new masterclass

---
+++ include/dg/llvm/analysis/ValueRelations/AnalysisGraph.hpp
@@ -0,0 +1,40 @@
+#ifndef _DG_LLVM_ANALYSIS_GRAPH_HPP_
+#define _DG_LLVM_ANALYSIS_GRAPH_HPP_
+
+#include <llvm/IR/Module.h>
+
+#include "GraphBuilder.hpp"
+
+#ifndef NDEBUG
+#include "getValName.h"
+#endif
+
+namespace dg {
+namespace analysis {
+namespace vr {
+
+class AnalysisGraph {
+
+    const llvm::Module& module;
+
+    // VRLocation corresponding to the state of the program BEFORE executing the instruction
+    std::map<const llvm::Instruction *, VRLocation *> locationMapping;
+    std::map<const llvm::BasicBlock *, std::unique_ptr<VRBBlock>> blockMapping;
+
+public:
+    AnalysisGraph(const llvm::Module& M) : module(M) {
+        GraphBuilder gb(module, locationMapping, blockMapping);
+        gb.build();
+        //graph analyzer
+    }
+
+    const std::map<const llvm::BasicBlock *, std::unique_ptr<VRBBlock>>& getBlockMapping() const {
+        return blockMapping;
+    }
+};
+
+} // namespace vr
+} // namespace analysis
+} // namespace dg
+
+#endif // _DG_LLVM_ANALYSIS_GRAPH_HPP_
