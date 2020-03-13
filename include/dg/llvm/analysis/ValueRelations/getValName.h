77e2838efe825be27ffc305f90e3e915c6d0565a
Anna Rechtackova <xrechtac@fi.muni.cz>
2020-03-13 20:15:00 +0100

    add function for printing type name

--- include/dg/llvm/analysis/ValueRelations/getValName.h
+++ include/dg/llvm/analysis/ValueRelations/getValName.h
@@ -25,6 +25,22 @@ inline std::string getValName(const llvm::Value *val) {
     return result;
 }
 
+inline std::string getTypeName(const llvm::Type* type) {
+    std::ostringstream ostr;
+    llvm::raw_os_ostream ro(ostr);
+
+    assert(type);
+    ro << *type;
+    ro.flush();
+
+    std::string result = ostr.str();
+    auto it = result.begin();
+    while (it != result.end() && *it == ' ') it = result.erase(it);
+
+    // break the string if it is too long
+    return result;  
+}
+
 } // namespace debug
 } // namespace dg
 
